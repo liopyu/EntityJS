43e9f403c49f6ddc1d7dbeadc73576c876a4038d
Notenoughmail <78008321+Notenoughmail@users.noreply.github.com>
2024-01-19 15:56:53 -0700

    Fixy

--- src/main/java/net/liopyu/entityjs/builders/ArrowEntityBuilder.java
+++ src/main/java/net/liopyu/entityjs/builders/ArrowEntityBuilder.java
@@ -2,32 +2,23 @@ package net.liopyu.entityjs.builders;
 
 import dev.latvian.mods.kubejs.registry.BuilderBase;
 import dev.latvian.mods.kubejs.registry.RegistryInfo;
-import dev.latvian.mods.kubejs.typings.Generics;
 import dev.latvian.mods.kubejs.typings.Info;
 import dev.latvian.mods.kubejs.typings.Param;
-import it.unimi.dsi.fastutil.booleans.BooleanPredicate;
-import net.liopyu.entityjs.entities.ArrowEntityJS;
 import net.liopyu.entityjs.entities.IArrowEntityJS;
 import net.liopyu.entityjs.item.ArrowItemBuilder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
-import net.minecraft.world.entity.projectile.Arrow;
-import net.minecraft.world.item.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BooleanSupplier;
-import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.Predicate;
 
-
+// TODO: Change this to a generic projectile builder, iirc items have to ba handled differently for arrows and other types
 public abstract class ArrowEntityBuilder<T extends AbstractArrow & IArrowEntityJS> extends BuilderBase<EntityType<T>> {
 
-    public transient ArrowItemBuilder getPickupItem;
     public static final List<ArrowEntityBuilder<?>> thisList = new ArrayList<>();
     public transient float width;
     public transient float height;
@@ -97,26 +88,6 @@ public abstract class ArrowEntityBuilder<T extends AbstractArrow & IArrowEntityJ
     }
 
     public abstract EntityType.EntityFactory<T> factory();
-
-    public transient ArrowEntityJSBuilder builder;
-
-    @Info(value = "Creates an arrow item for this entity type")
-    @Generics(value = {AbstractArrow.class, ArrowEntityBuilder.class})
-    public ArrowEntityBuilder<T> getPickupItem(Consumer<ArrowItemBuilder> getPickupItem) {
-
-        this.getPickupItem = new ArrowItemBuilder(id, this, builder);
-        getPickupItem.accept(this.getPickupItem);
-        
-        return this;
-    }
-
-
-    @Override
-    public void createAdditionalObjects() {
-        if (getPickupItem != null) {
-            RegistryInfo.ITEM.addBuilder(getPickupItem);
-        }
-    }
 
     @Override
     public EntityType<T> createObject() {
