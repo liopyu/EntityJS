dcf7f048ec39591a09c350bce0b9b73d1f54bbae
Liopyu <mistakensound@gmail.com>
2024-04-12 10:30:51 -0400

    updated cgm loading message

--- src/main/java/net/liopyu/entityjs/EntityJSPlugin.java
+++ src/main/java/net/liopyu/entityjs/EntityJSPlugin.java
@@ -60,7 +60,7 @@ public class EntityJSPlugin extends KubeJSPlugin {
         RegistryInfo.ENTITY_TYPE.addType("minecraft:eye_of_ender", EyeOfEnderJSBuilder.class, EyeOfEnderJSBuilder::new);
 
         if (Platform.isModLoaded("cgm") && Platform.isModLoaded("framework")) {
-            LOGGER.info("Loading CGM");
+            LOGGER.info("Loading CGM-EntityJS compatibility");
             RegistryInfo.ENTITY_TYPE.addType("cgm:missile", CGMProjectileEntityJSBuilder.class, CGMProjectileEntityJSBuilder::new);
         }
     }
