3e745a14ca805f39d25d29f100d81e003415c084
Liopyu <mistakensound@gmail.com>
2024-07-28 10:39:52 -0400

    changed onhitentity & onhitblock context to take in a Projectile instead of ThrowableItemProjectile to be more inclusive for addon compatibility, bumped version

--- src/main/java/net/liopyu/entityjs/util/ContextUtils.java
+++ src/main/java/net/liopyu/entityjs/util/ContextUtils.java
@@ -21,6 +21,7 @@ import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ProjectileWeaponItem;
@@ -966,12 +967,12 @@ public class ContextUtils {
 
     public static class ProjectileEntityHitContext {
         @Info("The projectile that was thrown")
-        public final ThrowableItemProjectile entity;
+        public final Projectile entity;
 
         @Info("The result of the hit")
         public final EntityHitResult result;
 
-        public ProjectileEntityHitContext(EntityHitResult result, ThrowableItemProjectile entity) {
+        public ProjectileEntityHitContext(EntityHitResult result, Projectile entity) {
             this.entity = entity;
             this.result = result;
         }
@@ -980,12 +981,12 @@ public class ContextUtils {
 
     public static class ProjectileBlockHitContext {
         @Info("The throwable item projectile that hit the block")
-        public final ThrowableItemProjectile entity;
+        public final Projectile entity;
 
         @Info("The result of the hit on the block")
         public final BlockHitResult result;
 
-        public ProjectileBlockHitContext(BlockHitResult result, ThrowableItemProjectile entity) {
+        public ProjectileBlockHitContext(BlockHitResult result, Projectile entity) {
             this.entity = entity;
             this.result = result;
         }
