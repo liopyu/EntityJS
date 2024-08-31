897a42a05c75ce8673395951de26f3e8f5664271
Liopyu <mistakensound@gmail.com>
2024-08-31 12:25:59 -0400

    fixed onInteract checking for wrong builder in EntityMixin

--- src/main/java/net/liopyu/entityjs/mixin/EntityMixin.java
+++ src/main/java/net/liopyu/entityjs/mixin/EntityMixin.java
@@ -501,8 +501,8 @@ public class EntityMixin {
     }
 
     @Inject(method = "interact", at = @At(value = "HEAD", ordinal = 0), remap = true, cancellable = true)
-    public void mobInteract(Player pPlayer, InteractionHand pHand, CallbackInfoReturnable<InteractionResult> cir) {
-        if (entityJs$builder != null && entityJs$builder instanceof ModifyMobBuilder builder) {
+    public void onInteract(Player pPlayer, InteractionHand pHand, CallbackInfoReturnable<InteractionResult> cir) {
+        if (entityJs$builder != null && entityJs$builder instanceof ModifyEntityBuilder builder) {
             if (builder.onInteract != null) {
                 final ContextUtils.EntityInteractContext context = new ContextUtils.EntityInteractContext(entityJs$getLivingEntity(), pPlayer, pHand);
                 EntityJSHelperClass.consumerCallback(builder.onInteract, context, "[EntityJS]: Error in " + entityJs$entityName() + "builder for field: onInteract.");
