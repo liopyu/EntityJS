1f55d31655b4168479ecda8bc0bb3af387436c59
Liopyu <mistakensound@gmail.com>
2024-09-08 20:31:01 -0400

    removed client only binding, version bump

--- src/main/java/net/liopyu/entityjs/EntityJSPlugin.java
+++ src/main/java/net/liopyu/entityjs/EntityJSPlugin.java
@@ -70,7 +70,6 @@ public class EntityJSPlugin extends KubeJSPlugin {
     @Override
     public void registerBindings(BindingsEvent event) {
         event.add("EntityJSUtils", EntityJSUtils.class);
-        event.add("RenderType", RenderType.class);
     }
 
     @Override
