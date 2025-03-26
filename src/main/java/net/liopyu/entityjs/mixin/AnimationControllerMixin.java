c942be7e0b8faeb24f9c374ae8e56c751de2a2cb
Liopyu <mistakensound@gmail.com>
2025-03-25 20:00:50 -0400

    - Added getCurrentAnimationTick() to Geckolib AnimationController giving the current tick the playing animation is at, Made `setCanShootFromDispenser()` return the builder instead of void allowing builder method chaining,added isMoving() to Entity class, version bump

---
+++ src/main/java/net/liopyu/entityjs/mixin/AnimationControllerMixin.java
@@ -0,0 +1,54 @@
+package net.liopyu.entityjs.mixin;
+
+import net.liopyu.entityjs.util.implementation.IAnimationControllerJS;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import software.bernie.geckolib.core.animatable.GeoAnimatable;
+import software.bernie.geckolib.core.animatable.model.CoreGeoBone;
+import software.bernie.geckolib.core.animatable.model.CoreGeoModel;
+import software.bernie.geckolib.core.animation.AnimationController;
+import software.bernie.geckolib.core.animation.AnimationState;
+import software.bernie.geckolib.core.state.BoneSnapshot;
+
+import java.util.Map;
+import java.util.function.Function;
+
+@Mixin(value = AnimationController.class, remap = false)
+public class AnimationControllerMixin<T extends GeoAnimatable> implements IAnimationControllerJS {
+    @Unique
+    private double entityJs$currentAnimationTick = 0;
+    @Unique
+    private AnimationController<T> entityJs$self = (AnimationController<T>) (Object) this;
+    @Shadow
+    protected boolean shouldResetTick;
+    @Shadow
+    protected Function<T, Double> animationSpeedModifier;
+    @Final
+    @Shadow
+    protected T animatable;
+    @Shadow
+    protected double tickOffset;
+
+    @Unique
+    protected double entityJs$adjustTick(double tick) {
+        if (!this.shouldResetTick)
+            return this.animationSpeedModifier.apply(this.animatable) * Math.max(tick - this.tickOffset, 0);
+        return 0;
+    }
+
+    @Inject(method = "process", at = @At(value = "HEAD"))
+    private void entityJs$onProcess(CoreGeoModel<T> model, AnimationState<T> state, Map<String, CoreGeoBone> bones, Map<String, BoneSnapshot> snapshots, double seekTime, boolean crashWhenCantFindBone, CallbackInfo ci) {
+
+        this.entityJs$currentAnimationTick = this.entityJs$adjustTick(seekTime);
+    }
+
+    public double entityJs$getCurrentAnimationTick() {
+        return this.entityJs$currentAnimationTick;
+    }
+}
+
---
+++ src/main/java/net/liopyu/entityjs/util/implementation/IAnimationControllerJS.java
@@ -0,0 +1,8 @@
+package net.liopyu.entityjs.util.implementation;
+
+import dev.latvian.mods.rhino.util.RemapPrefixForJS;
+
+@RemapPrefixForJS("entityJs$")
+public interface IAnimationControllerJS {
+    double entityJs$getCurrentAnimationTick();
+}
