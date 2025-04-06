0372fd9bcdce856f3b17fdac419c1cf5273873ae
Liopyu <mistakensound@gmail.com>
2025-04-06 15:10:51 -0400

    - Added `defineSyncedData` to ModifyLivingEntityBuilder, added `addSyncedData` & `getSyncedData` & `setSyncedData` to LivingEntity providing scripters a way to add auto client/server synced nbt to any entity, version bump

--- src/main/java/net/liopyu/entityjs/builders/modification/ModifyLivingEntityBuilder.java
+++ src/main/java/net/liopyu/entityjs/builders/modification/ModifyLivingEntityBuilder.java
@@ -88,11 +88,16 @@ public class ModifyLivingEntityBuilder extends ModifyEntityBuilder {
     public transient MobType mobType;
     public transient Function<Entity, Object> nextStep;
     public final List<ContextUtils.PartEntityParams<?>> partEntityParamsList = new ArrayList<>();
+    public transient Consumer<LivingEntity> defineSyncedData;
 
     public ModifyLivingEntityBuilder(EntityType<?> entityType) {
         super(entityType);
     }
 
+    public ModifyEntityBuilder defineSyncedData(Consumer<LivingEntity> consumer) {
+        this.defineSyncedData = consumer;
+        return this;
+    }
    /* @Info(value = """
             Adds an extra hitbox to the mob. Aka part-entities.
             Vanilla ticks extra hitboxes(for example the ender dragon's) with the
---
+++ src/main/java/net/liopyu/entityjs/util/implementation/ILivingEntityJS.java
@@ -0,0 +1,16 @@
+package net.liopyu.entityjs.util.implementation;
+
+import dev.latvian.mods.rhino.util.RemapPrefixForJS;
+import net.liopyu.entityjs.util.EntitySerializerType;
+
+@RemapPrefixForJS("entityJs$")
+public interface ILivingEntityJS {
+    void entityJs$addSyncedData(EntitySerializerType type, String key, Object value);
+
+    <T> T entityJs$getSyncedData(String identifier);
+
+    void entityJs$addSyncedData(String identifier, Object value);
+
+    void entityJs$setSyncedData(String key, Object value);
+}
+
