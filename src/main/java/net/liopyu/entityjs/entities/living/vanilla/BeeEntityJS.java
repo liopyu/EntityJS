60ec6ee75f16425f70cb50544acd343a1fb5c031
Liopyu <mistakensound@gmail.com>
2025-05-29 11:39:34 -0400

    - Fixed crash when disabling goals for custom Bee entities - Added getEntityBuilder to EntityJSUtils

--- src/main/java/net/liopyu/entityjs/entities/living/vanilla/BeeEntityJS.java
+++ src/main/java/net/liopyu/entityjs/entities/living/vanilla/BeeEntityJS.java
@@ -682,7 +682,12 @@ public class BeeEntityJS extends Bee implements IAnimatableJS {
         super.onAddedToWorld();
         if (builder.defaultGoals) {
             super.registerGoals();
+        } else {
+            this.beePollinateGoal = new BeePollinateGoal();
+            this.goToHiveGoal = new BeeGoToHiveGoal();
+            this.goToKnownFlowerGoal = new BeeGoToKnownFlowerGoal();
         }
+
         if (builder.onAddedToWorld != null) {
             EntityJSHelperClass.consumerCallback(builder.onAddedToWorld, this, "[EntityJS]: Error in " + entityName() + "builder for field: onAddedToWorld.");
 
