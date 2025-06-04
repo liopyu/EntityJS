dca7e319e167a494956ad78988fea0b8be991d69
Liopyu <mistakensound@gmail.com>
2025-06-04 18:00:35 -0400

    more CustomEntity rendering optimizations

--- src/main/java/net/liopyu/entityjs/mixin/RegistryEventJSMixin.java
+++ src/main/java/net/liopyu/entityjs/mixin/RegistryEventJSMixin.java
@@ -7,20 +7,14 @@ import dev.latvian.mods.kubejs.registry.RegistryInfo;
 import dev.latvian.mods.kubejs.script.ScriptType;
 import dev.latvian.mods.kubejs.util.UtilsJS;
 import net.liopyu.entityjs.builders.misc.CustomEntityBuilder;
-import net.liopyu.entityjs.events.EntityRegistryEvent;
-import net.liopyu.entityjs.util.EventHandlers;
 import net.liopyu.entityjs.util.implementation.IRegistryJS;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
-import java.util.function.Consumer;
 
 @Mixin(RegistryEventJS.class)
 public class RegistryEventJSMixin<T> implements IRegistryJS {
@@ -39,7 +33,6 @@ public class RegistryEventJSMixin<T> implements IRegistryJS {
         CustomEntityBuilder b = null;
         if (LivingEntity.class.isAssignableFrom(entityClass)) {
             b = new CustomEntityBuilder(rl, (Class<? extends LivingEntity>) entityClass);
-            //consumer.accept(b);
         }
         if (b == null) {
             throw new IllegalArgumentException("CustomEntityBuilder is null for entity id: " + id);
