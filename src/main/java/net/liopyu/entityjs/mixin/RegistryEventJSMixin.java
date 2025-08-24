6937c112dc00b1c492a34d2c9a305cf46e606be6
Liopyu <mistakensound@gmail.com>
2025-08-24 13:34:23 -0400

    - Changed createCustom method to only accept LivingEntity classes

--- src/main/java/net/liopyu/entityjs/mixin/RegistryEventJSMixin.java
+++ src/main/java/net/liopyu/entityjs/mixin/RegistryEventJSMixin.java
@@ -31,7 +31,7 @@ public class RegistryEventJSMixin<T> implements IRegistryJS {
     public List<BuilderBase<? extends T>> created;
 
     @Info(value = """
-            Creates a new custom entity based on an existing entity class.
+            Creates a new custom entity based on an existing living entity class.
             This allows extending or modifying behavior of vanilla or modded entities dynamically.
             
             The builder provided in the callback can be used to directly access the respective entity's modification builder.
@@ -47,9 +47,9 @@ public class RegistryEventJSMixin<T> implements IRegistryJS {
             ```
             """
     )
-    public CustomEntityBuilder entityJs$createCustom(String id, Class<? extends Entity> entityClass, Consumer<ModifyEntityBuilder> consumer) {
-        if (!Entity.class.isAssignableFrom(entityClass)) {
-            throw new IllegalArgumentException("Tried to create entity from a class that does not extend Entity. Id: " + id);
+    public CustomEntityBuilder entityJs$createCustom(String id, Class<? extends LivingEntity> entityClass, Consumer<ModifyEntityBuilder> consumer) {
+        if (!LivingEntity.class.isAssignableFrom(entityClass)) {
+            throw new IllegalArgumentException("Tried to create entity from a class that does not extend LivingEntity. Id: " + id);
         }
         var rl = UtilsJS.getMCID(ScriptType.STARTUP.manager.get().context, KubeJS.appendModId(id));
         CustomEntityBuilder b = null;
--- src/main/java/net/liopyu/entityjs/util/implementation/IRegistryJS.java
+++ src/main/java/net/liopyu/entityjs/util/implementation/IRegistryJS.java
@@ -4,10 +4,11 @@ import dev.latvian.mods.rhino.util.RemapPrefixForJS;
 import net.liopyu.entityjs.builders.misc.CustomEntityBuilder;
 import net.liopyu.entityjs.builders.modification.ModifyEntityBuilder;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 
 import java.util.function.Consumer;
 
 @RemapPrefixForJS("entityJs$")
 public interface IRegistryJS {
-    CustomEntityBuilder entityJs$createCustom(String id, Class<? extends Entity> entityClass, Consumer<ModifyEntityBuilder> consumer);
+    CustomEntityBuilder entityJs$createCustom(String id, Class<? extends LivingEntity> entityClass, Consumer<ModifyEntityBuilder> consumer);
 }
